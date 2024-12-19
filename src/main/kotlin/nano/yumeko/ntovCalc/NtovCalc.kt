18a26131b28d745992509064c8da48260d0b4a4c
Qiong-Mengzi <2860793267@qq.com>
2024-12-19 20:49:15 +0800

    init

---
+++ src/main/kotlin/nano/yumeko/ntovCalc/NtovCalc.kt
@@ -0,0 +1,14 @@
+package nano.yumeko.ntovCalc
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NtovCalc : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
